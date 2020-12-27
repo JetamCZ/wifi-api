47de55ab6fde85998c417a616882ad7ebb266cd9
Matěj Půhoný <info@jetam.eu>
2020-12-27 14:54:45 +0100

    fix beacon path

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ app.use("/auth/create-org", apiLimiter);
 app.use((req, res, next) => {
     req.token = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : ""
 
-    if(!req.path.includes('/auth')) {
+    if(!req.path.includes('/auth') && req.path !== '/beacon') {
         if(req.token) {
             try {
                 const data = jwt.verify(req.token, process.env.JWT_TOKEN)
@@ -70,7 +70,6 @@ app.use((req, res, next) => {
         }
         next()
     }
-
 })
 
 app.get('/token', (req, res) => {
