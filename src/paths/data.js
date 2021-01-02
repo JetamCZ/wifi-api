061eb268b832a9ef423ac99d9a1820c87d8b751d
Matěj Půhoný <info@jetam.eu>
2021-01-02 15:00:33 +0100

    update

---
+++ src/paths/data.js
@@ -0,0 +1,21 @@
+const BeaconController = require('../controllers/BeaconController')
+const MeetController = require('../controllers/MeetController')
+
+module.exports = {
+    post: async (req, res) => {
+        console.log('DATA', req.body.device_key)
+
+        await BeaconController.updateLastSeen(req.body.device_key)
+
+        const devices = req.body.devices.map(dev => {
+            dev.mac = dev.mac.toLowerCase()
+            return dev
+        })
+
+        for (let i = 0; i < req.body.devices.length; i++) {
+            await MeetController.saveMeet(req.body.device_key, devices[i].mac, devices[i].rssi)
+        }
+
+        res.status(202).send()
+    }
+}
