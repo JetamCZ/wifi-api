b76eb6056f41834cb36fd9bee55d292952b682c1
Matěj Půhoný <info@jetam.eu>
2021-02-11 09:14:45 +0100

    cors problem

--- src/app.js
+++ src/app.js
@@ -41,12 +41,6 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
-
 const fr = require("./utils/fr")
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(fr.getSwaggerMegaFile()))
 
