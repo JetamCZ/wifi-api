b891744afee5fa6f1c6a21b8e3e4290aeab9f506
Matěj Půhoný <info@jetam.eu>
2021-03-01 21:45:09 +0100

    autoswag

--- src/autoSwagger/app.js
+++ src/autoSwagger/app.js
@@ -6,12 +6,13 @@ const fs = require("fs")
 const endpoints = require("./endpointsToAutoSwag")
 
 const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVmZjljMWQwMjM2YjFmNzA4MGQyZmRlMiIsImVtYWlsIjoiZEBkIiwibmFtZSI6IkthcmVsIE5vdsOhayJ9LCJvcmdhbml6YXRpb24iOnsiX2lkIjoiNWZmOWMxN2EyMzZiMWY3MDgwZDJmZGUwIiwibmFtZSI6IkRFTFRBIFPFoElFIn0sImlhdCI6MTYxMjE5MDk1OSwiZXhwIjoxNjEzMDU0OTU5fQ.-d7WwVHf-KHUXY59moSiHm_FfwNA4Q0NqHQugiCsiPE"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYwMWZlZjk2MGJmNzk4MThkOGRhMzQ2ZiIsImVtYWlsIjoibUBtIiwibmFtZSI6Ik1hdMSbaiBQxa9ob27DvSJ9LCJvcmdhbml6YXRpb24iOnsiX2lkIjoiNjAxZmVmODYwYmY3OTgxOGQ4ZGEzNDZkIiwibmFtZSI6Ik9TSUNFIn0sImlhdCI6MTYxNDYxNDkxNywiZXhwIjoxNjE1NDc4OTE3fQ.DM3bETt28CieZ5rZxZWZLy0R_44AgdVM_T8KyQ3nIoE"
+
 
 const replacePaths = {
-    "/localization/{id}": "/localization/5ffcc2946270ec2da06d653e",
-    "/devices/{id}": "/devices/5ffd51c84411eb4b040df3f9",
-    "/history/localization/{id}": "/history/localization/5ffcc2946270ec2da06d653e"
+    "/localization/{id}": "/localization/60206249b697774c304f8318",
+    "/devices/{id}": "/devices/6020ecfb080fc40564914fb0",
+    "/history/localization/{id}": "/history/localization/60206249b697774c304f8318"
 }
 
 try {
